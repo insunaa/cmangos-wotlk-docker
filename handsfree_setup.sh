0e16611fb3d8534862703cc8af1f165b1ef5f6f6
insunaa <insuna@no.capfr.fr>
2025-10-31 20:09:21 +0100

    Experimental: Add hands-free setup and update

---
+++ handsfree_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ "$#" -lt 1 ]; then
+    echo 'Usage: ./handsfree_setup.sh /path/to/your/wow/client'
+    exit 1
+fi
+
+if [[ ! -d "$1" ]]; then
+    echo 'Target must be the `World of Warcraft` directory.'
+    exit 1
+fi
+
+if [ "$1" = "/" ] || [ "$1" = "" ]; then
+    echo 'Path cannot be root or be empty.'
+    exit 1
+fi
+
+if [[ ! -d "$1/Data" ]] && [[ ! -d "$1/data" ]]; then
+    echo 'Target must be the `World of Warcraft` directory.'
+    exit 1
+fi
+
+if [ ! -d data ]; then
+    mkdir data
+fi
+
+bash build_bots.sh
+bash update_dbs.sh
+bash extract.sh $1
+
+echo 'Setup finished! Edit `etc/playerbot.conf` to optionally disable playerbots.'
+echo 'To create your account use `podman-compose up -d` or `docker compose up -d` to start the composition, then run `enter_console.sh` to log into the terminal.'
---
+++ handsfree_update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo 'Updating the core.'
+sleep 3s
+bash build_bots.sh
+
+read -p "Also update the Databases? [y/N] " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+if [ ! -d databases ]; then
+    echo 'No databases to update found'
+    exit 1
+fi
+
+if [ -d databases_old ]; then
+    echo '`databases_old` directory found. Please rename or delete it.'
+    exit 1
+fi
+
+echo 'Updating all Databases. If errors occur, delete the `databases` directory and rename `databases_old` to `databases`'
+sleep 3s
+bash db_backup.sh
+mv databases databases_old
+bash update_dbs.sh
+bash db_restore.sh
