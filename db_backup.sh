262b07202d83937ad3cd6ddfbf8c3d94fbd5f502
insunaa <insuna@no.capfr.fr>
2025-10-31 13:07:13 +0100

    Add script to backup realmd and characters db content

---
+++ db_backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Backing up realmd Database"
+for t in $(sqlite3 databases/wotlkrealmd.sqlite ".mode list" "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%';"); do
+  echo "-- $t"
+  sqlite3 databases/wotlkrealmd.sqlite ".mode insert $t" ".headers on" "SELECT * FROM \"$t\";"
+done > realmd_backup.sql
+
+echo "Backing up characters Database"
+for t in $(sqlite3 databases/wotlkcharacters.sqlite ".mode list" "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%';"); do
+  echo "-- $t"
+  sqlite3 databases/wotlkcharacters.sqlite ".mode insert $t" ".headers on" "SELECT * FROM \"$t\";"
+done > characters_backup.sql
