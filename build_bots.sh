240f5bc5b8b464c43d68a1c6b10195cd48a8f769
insunaa <insuna@no.capfr.fr>
2025-11-02 23:24:48 +0100

    cache optimizations part 2

--- build_bots.sh
+++ build_bots.sh
@@ -22,3 +22,5 @@ else
 fi
 
 $ORCH build --build-arg AHBOT=1 --build-arg PLAYERBOTS=1 --build-arg INVALIDATE_CACHE="$(date)" -t cmangos-wotlk:latest .
+
+$ORCH rmi $($ORCH images --filter label=deleteme=true -q)
--- build_default.sh
+++ build_default.sh
@@ -22,3 +22,5 @@ else
 fi
 
 $ORCH build --build-arg AHBOT=0 --build-arg PLAYERBOTS=0 --build-arg INVALIDATE_CACHE="$(date)" -t mangos-wotlk:latest .
+
+$ORCH rmi $($ORCH images --filter label=deleteme=true -q)
